bad8bed187e37e41154754fbf27892d862d4a04e
Dmitry Ishchenko <to.dmitry.ishchenko@gmail.com>
2021-08-13 11:51:03 +0300

    Add filter reducer to the store

--- src/app/store.js
+++ src/app/store.js
@@ -1,8 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit';
-import ticketsReducer from "../features/tickets/ticketsSlice"
+import ticketsReducer from "../features/tickets/ticketsSlice";
+import filtersReducer from "../features/tickets/filtersSlice"
 
 export const store = configureStore({
   reducer: {
-    tickets: ticketsReducer
+    tickets: ticketsReducer,
+    filters: filtersReducer
   }
 });
