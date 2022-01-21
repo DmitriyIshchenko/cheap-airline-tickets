6fc2a865dbde9e57b572f531e6a926fe85fe4352
Dmitry Ishchenko <to.dmitry.ishchenko@gmail.com>
2022-01-21 21:56:25 +0300

    Add custom checkboxes

--- src/features/tickets/Filters.js
+++ src/features/tickets/Filters.js
@@ -1,39 +1,115 @@
-import { useState } from "react";
-import { useDispatch } from "react-redux"
-import { toggleFilter } from "./filtersSlice"
-import "./Filters.css"
-export function Filters() {
+import React, { useEffect, useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { setSortBy, setStopsFilter, filterTickets } from './ticketsSlice';
+
+import "../../styles/Filters.css"
+
+
+export function SortContols() {
+
     const dispatch = useDispatch();
-    const [checkedState, setCheckedState] = useState([true, true, false, false]);
-    const stops = ["Без пересадок", "1 пересадка", "2 пересадки", "3 пересадки"];
-    const handleOnChange = (position) => {
-        const updatedCheckedState = checkedState.map((item, index) =>
-            index === position ? !item : item
-        );
-        setCheckedState(updatedCheckedState);
-        dispatch(toggleFilter(position))
+
+    const targetFied = useSelector(state => state.tickets.sortedBy)
+
+    const handleChange = (e) => {
+        dispatch(setSortBy(e.target.value));
+        dispatch(filterTickets())
     }
-    return (
-        <div id="filters-container">
-            <p id="filters-title">КОЛИЧЕСТВО ПЕРЕСАДОК</p>
-            <ul>
-                {stops.map((item, index) => {
-                    return (
-                        <li key={index}>
-                            <div className="container">
-                                <input
-                                    className="checkbox"
-                                    type="checkbox"
-                                    name={"filter-" + index}
-                                    checked={checkedState[index]}
-                                    onChange={() => handleOnChange(index)}
-                                />
-                                <label className="filters-label" for={"filter-" + index}>{item}</label>
-                            </div>
-                        </li>
-                    )
-                })}
-            </ul>
-        </div>
-    )
+
+    return <div className='sort-container'>
+        <input
+            type="radio"
+            name="sort"
+            id="byDuration"
+            value="duration"
+            checked={targetFied === "duration"}
+            onChange={handleChange}
+            className='radio'
+        />
+        <label
+            htmlFor="byDuration"
+            className={`${targetFied === "duration" ? "sort-selected" : ""} sort sort-left`}>САМЫЙ БЫСТРЫЙ</label>
+        <input
+            type="radio"
+            name="sort"
+            id="byPrice"
+            value="price"
+            checked={targetFied === "price"}
+            onChange={handleChange}
+            className='radio' />
+        <label
+            htmlFor="byPrice"
+            className={`${targetFied === "price" ? "sort-selected" : ""} sort sort-right`}>САМЫЙ ДЕШЕВЫЙ</label>
+    </div>;
 }
+
+export function FiltersContorls() {
+
+    const dispatch = useDispatch();
+
+    const [isAllSelected, toggleSelectAll] = useState(false);
+    const [filters, setFilters] = useState({
+        "0": true,
+        "1": true,
+        "2": true,
+        "3": true
+    })
+
+    const handleChangeAll = () => {
+        if (!isAllSelected) {
+            const allTrueObj = {};
+            for (let key in filters) {
+                allTrueObj[key] = true;
+            }
+            setFilters(allTrueObj);
+            toggleSelectAll(true);
+        }
+    }
+    const handleChange = (e) => {
+        setFilters({ ...filters, [e.target.value]: !filters[e.target.value] });
+    }
+
+    useEffect(() => {
+        dispatch(setStopsFilter(Object.keys(filters).filter(key => filters[key]).map(key => +key)));
+        dispatch(filterTickets())
+        if (Object.values(filters).every(value => value)) {
+            toggleSelectAll(true);
+        } else toggleSelectAll(false);
+    }, [filters, dispatch])
+
+    const checkboxes = Object.entries(filters).map(([name, value]) => {
+        return (<li key={name}>
+            <label htmlFor={name} className='checkbox-container'>
+                {+name === 0 ? "Без пересадок" : +name > 1 ? `${name} пересадки` : "1 пересадка"}
+                <input
+                    type="checkbox"
+                    name="stops"
+                    id={name}
+                    value={name}
+                    checked={value}
+                    onChange={handleChange} />
+                <span className="checkmark"></span>
+            </label>
+        </li>)
+    });
+
+    return <div className='stops-container'>
+        <p className='stops-container-title'>Количество пересадок</p>
+        <ul>
+            <li key="all">
+                <label htmlFor="all" className='checkbox-container'>
+                    Все
+                    <input
+                        type="checkbox"
+                        name="stops"
+                        id="all"
+                        value="all"
+                        checked={isAllSelected} onChange={handleChangeAll} />
+                    <span className="checkmark"></span>
+                </label>
+            </li>
+            {checkboxes}
+        </ul>
+    </div>;
+}
+
