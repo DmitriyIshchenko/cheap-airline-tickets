6c86a7ece23fbd8efee2b799dfc9d0115a76fce6
Dmitry Ishchenko <to.dmitry.ishchenko@gmail.com>
2022-02-10 15:10:21 +0300

    Rename function

--- src/features/tickets/ticketsAPI.js
+++ src/features/tickets/ticketsAPI.js
@@ -1,6 +1,6 @@
-export const fetchTicketsAsync = async () => {
-    const searchId = await fetch("https://front-test.beta.aviasales.ru/search")
-        .then(response => response.json()).then(data => data.searchId);
-    return fetch(`https://front-test.beta.aviasales.ru/tickets?searchId=${searchId}`)
-        .then(response => response.json()).then(data => data.tickets);
+export const fetchTickets = async () => {
+  const searchId = await fetch("https://front-test.beta.aviasales.ru/search")
+    .then(response => response.json()).then(data => data.searchId);
+  return fetch(`https://front-test.beta.aviasales.ru/tickets?searchId=${searchId}`)
+    .then(response => response.json()).then(data => data.tickets);
 }
